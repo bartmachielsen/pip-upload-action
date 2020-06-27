b487a8821f742b9d788af3c06bb1645667273279
onichandame <zxinmyth@gmail.com>
2020-06-27 10:50:38 +0000

    test;

--- run.sh
+++ run.sh
@@ -1,19 +1,25 @@
 #!/bin/sh -l
 
 # cd to repo
-cd $GITHUB_WORKSPACE/$1
+REPO=$GITHUB_WORKSPACE/$1
+cd $REPO
+echo current directory $REPO
 
 # install dependencies
+echo installing packaging tools
 pip3 install wheel twine
 if [ "NONE" = "$2" ]
 then
     echo "Skipping installation of dependencies as requested by user"
 else
+  echo installing dependencies in $2
     pip3 install -r $2
 fi
 
 # build
+echo bundling package
 python3 $3 sdist bdist_wheel
 
 # upload
+echo uploading package
 twine upload --username $4 --password $5 dist/*
